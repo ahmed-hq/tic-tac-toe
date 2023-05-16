cfb66d7db4705dce5ec0f3504ccfe3713f8de8e2
Ahmed Walid <123060771+ahmed-hq@users.noreply.github.com>
2023-05-16 21:09:28 +0300

    Adding Start Btn

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
-const Player = (sign) => {
-  const playerSign = sign;
+const Player = (s) => {
+  const playerSign = s;
   return { playerSign };
 };
+
+// const Btn = () => {
+//   const xBtn = document.querySelector('.x_option-btn');
+//   const oBtn = document.querySelector('.o_option-btn');
+//   const pvpBtn = document.querySelector('.pvp-btn');
+
+//   const xActive = () => {
+//     console.log('jjjj');
+//   };
+
+//   return { xActive };
+// };
+
 const Gameboard = (() => {
   const gameBoard = ['', '', '', '', '', '', '', '', ''];
+  let sign;
+  let mode;
+
+  const xBtn = document.querySelector('.x_option-btn');
+  const oBtn = document.querySelector('.o_option-btn');
+  const pvpBtn = document.querySelector('.pvp-btn');
+
+  pvpBtn.addEventListener('click', () => {
+    mode = 'pvp';
+    pvpBtn.style.opacity = '75%';
+  });
+  xBtn.addEventListener('click', () => {
+    sign = 'X';
+    xBtn.style.opacity = '75%';
+    oBtn.style.opacity = '100%';
+    console.log(sign);
+  });
+  oBtn.addEventListener('click', () => {
+    sign = 'O';
+    xBtn.style.opacity = '100%';
+    oBtn.style.opacity = '75%';
+    console.log(sign);
+  });
+
+  const controllerChoice = () => {};
 
   const createBlocks = () => {
     const gameBoardWrapper = document.querySelector('.game_board-wrapper');
