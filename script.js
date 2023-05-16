46cad62e077df233156ac06eb38cd0d7bee2bc61
Ahmed Walid <123060771+ahmed-hq@users.noreply.github.com>
2023-05-16 09:37:37 +0300

    Creating create blocks function

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
-const Player = (playerSign) => {
-  const sign = playerSign;
-  return { sign };
+const Player = (sign) => {
+  const playerSign = sign;
+  return { playerSign };
 };
 const Gameboard = (() => {
   const gameBoard = ['', '', '', '', '', '', '', '', ''];
 
-  const render = (playerSign) => {
-    for (let i = 0; i < gameBoard.length; i++) {
-      document.createElement('div');
-      document.setAttribute('class', 'squares');
-      document.setAttribute('id', `square-${i}`);
-      squares.textContent = `${playerSign}`;
-      document.appendChild(squares);
-    }
+  const createBlocks = () => {
+    const container = document.querySelector('.container');
+    const block = document.createElement('div');
+    block.setAttribute('class', 'block');
+    container.appendChild(block);
   };
-  // for (let i = 0; i < gameBoard.length; i++) {
-  //   document.createElement('div');
-  //   document.setAttribute('class', 'squares');
-  //   document.setAttribute('id', `square-${i}`);
-  //   squares.textContent = `${playerSign}`;
-  //   document.appendChild(squares);
-  // }
-  gameBoard.forEach((playerSign) => {});
 
+  const render = () => {
+    gameBoard.forEach(createBlocks);
+  };
+
+  render();
   return {};
 })();
+
+// const container = document.querySelector('')
+/// ////////////////////////////////////////////////////////
+// const Gameboard = (() => {
+//   const gameBoard = ['', '', '', '', '', '', '', '', ''];
+
+//   const render = (playerSign) => {
+//     for (let i = 0; i < gameBoard.length; i++) {
+//       document.createElement('div');
+//       document.setAttribute('class', 'squares');
+//       document.setAttribute('id', `square-${i}`);
+//       squares.textContent = `${playerSign}`;
+//       document.appendChild(squares);
+//     }
+//   };
+//   gameBoard.forEach((playerSign) => {});
+
+//   return {};
+// })();
