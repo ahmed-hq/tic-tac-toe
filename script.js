32cac154cfa2eb64ffd06842bc6649862eecfbe9
Ahmed Walid <123060771+ahmed-hq@users.noreply.github.com>
2023-05-17 10:40:41 +0300

    Adding all elements in one module

--- script.js
+++ script.js
@@ -1,51 +1,71 @@
+const Element = (() => {
+  const modeOption = document.querySelector('.mode_options-wrapper');
+  const gameBoardWrapper = document.querySelector('.game_board-wrapper');
+  const xBtn = document.querySelector('.x_option-btn');
+  const oBtn = document.querySelector('.o_option-btn');
+  const pvpBtn = document.querySelector('.pvp-btn');
+  const botBtn = document.querySelector('.bot-btn');
+  const startBtn = document.querySelector('.start-btn');
+
+  return { gameBoardWrapper, xBtn, oBtn, pvpBtn, botBtn, startBtn };
+})();
+
 const Player = (s) => {
   const playerSign = s;
   return { playerSign };
 };
 
-// const Btn = () => {
-//   const xBtn = document.querySelector('.x_option-btn');
-//   const oBtn = document.querySelector('.o_option-btn');
-//   const pvpBtn = document.querySelector('.pvp-btn');
+const Btn = (btn1, btn2) => {
+  const modeActivation = () => {
+    btn1.addEventListener('click', () => {
+      mode = btn1.innerText;
+      btn1.style.opacity = '75%';
+      btn2.style.opacity = '100%';
+      console.log(mode);
+    });
+  };
+  const signActivation = () => {
+    btn1.addEventListener('click', () => {
+      sign = btn1.innerText;
+      btn1.style.opacity = '75%';
+      btn2.style.opacity = '100%';
+      console.log(sign);
+    });
+  };
+  const startActivation = () => {
+    btn1.addEventListener('click', () => {
+      console.log('fool');
+      Element.gameBoardWrapper.style.display = 'flex';
+      Element.startBtn.style.display = 'none';
+    });
+  };
 
-//   const xActive = () => {
-//     console.log('jjjj');
-//   };
+  return { modeActivation, signActivation, startActivation };
+};
 
-//   return { xActive };
-// };
-
-const Gameboard = (() => {
+const board = (() => {
   const gameBoard = ['', '', '', '', '', '', '', '', ''];
   let sign;
   let mode;
 
-  const xBtn = document.querySelector('.x_option-btn');
-  const oBtn = document.querySelector('.o_option-btn');
-  const pvpBtn = document.querySelector('.pvp-btn');
-  const botBtn = document.querySelector('.bot-btn');
+  const pvp = Btn(Element.pvpBtn, Element.botBtn);
+  const bot = Btn(Element.botBtn, Element.pvpBtn);
+  const x = Btn(Element.xBtn, Element.oBtn);
+  const o = Btn(Element.oBtn, Element.xBtn);
+  const start = Btn(Element.startBtn);
 
-  const btn = (btn1, btn2, type) => {
-    btn1.addEventListener('click', () => {
-      type = btn1.innerText;
-      btn1.style.opacity = '75%';
-      btn2.style.opacity = '100%';
-      console.log(type);
-    });
-  };
-
-  btn(pvpBtn, botBtn, mode);
-  btn(botBtn, pvpBtn, mode);
-  btn(xBtn, oBtn, sign);
-  btn(oBtn, xBtn, sign);
+  pvp.modeActivation();
+  bot.modeActivation();
+  x.signActivation();
+  o.signActivation();
+  start.startActivation();
 
   const controllerChoice = () => {};
 
   const createBlocks = () => {
-    const gameBoardWrapper = document.querySelector('.game_board-wrapper');
     const block = document.createElement('div');
     block.setAttribute('class', 'block');
-    gameBoardWrapper.appendChild(block);
+    Element.gameBoardWrapper.appendChild(block);
   };
 
   const render = () => {
